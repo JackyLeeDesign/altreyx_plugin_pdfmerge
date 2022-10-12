08c276e2049dbef91b98ac8d7916209a58b9fae4
PWCTW\JYKL <jackyleeidea@gmail.com>
2022-10-12 11:23:45 +0800

    feat:以production模式建立安裝，提高執行速度

--- src/App.vue
+++ src/App.vue
@@ -51,7 +51,7 @@
   </div>
 
   <footer class="footer mt-auto">
-    <p class="text-muted" style="margin: 0px;text-align: center;">版本：1.0.0</p>
+    <p class="text-muted" style="margin: 0px;text-align: center;">版本：0.0.5</p>
   </footer>
 
 </template>
