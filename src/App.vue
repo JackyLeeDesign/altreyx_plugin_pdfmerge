fba85296d5290fd1143ac4a21e5a9132c7c0b569
PWCTW\JYKL <jackyleeidea@gmail.com>
2022-10-17 08:54:04 +0800

    feature:調整合併元件操作方式, 按順序選擇多個PDF檔後執行

--- src/App.vue
+++ src/App.vue
@@ -8,48 +8,48 @@
         </div>
       </div>
     </div>
-    <div>{{showData}}</div>
     <!-- 主要內容 -->
     <div class="row">
       <div class="col">
         <!-- 第一步 -->
         <div class="card" style="margin-top:10px;">
-          <div class="card-header d-flex justify-content-between align-items-center"><b>請連結欲合併的PDF檔資料：</b></div>
+          <div class="card-header d-flex justify-content-between align-items-center"><b>Step1：請先選擇結果檔案存放資料夾：</b></div>
+          <div class="card-header d-flex justify-content-between align-items-center">(檔名自動儲存為 merge_result.pdf)</div>
           <div class="card-body" style="overflow-x:auto;">
-            <div class="mb-3">
-              <b v-if="legder_status === true" style="color:green;"><BIconCheckLg style="vertical-align:text-top;" class="icon" /> 已連接完成</b>
-              <b v-if="legder_status !== true" style="color:red;"><BIconExclamationTriangleFill style="vertical-align:text-top;" class="icon" /> {{legder_status}}</b>
-            </div>
+            <ayx data-ui-props="{type:'FileBrowse', widgetId:'outputPath',browseType:'Folder'}"></ayx>
           </div>
         </div>
 
         <!-- 第二步 -->
         <div class="card" style="margin-top:10px;">
-          <div class="card-header d-flex justify-content-between align-items-center"><b>Step3：請選擇排序順序</b></div>
+          <div class="card-header d-flex justify-content-between align-items-center"><b>Step2：請依照合併順序選擇欲合併之PDF檔案:</b>
+          </div>
+          <div class="card-header d-flex justify-content-between align-items-center">(一次最多合併8個檔案，若超過則重複執行該元件即可)</div>
           <div class="card-body" style="overflow-x:auto;">
-            <draggable :list="fileList" :disabled="!enabledSortList" item-key="name" class="list-group"
-              ghost-class="ghost" :move="checkMove" @start="dragging = true" @end="dragging = false">
-              <template #item="{ element }">
-                <div class="list-group-item" :class="{ 'not-draggable': !enabled }" :style="{'cursor': 'move'}">
-                  {{ element.name }}
-                </div>
-              </template>
-            </draggable>
-            <!-- <div class="form-check mb-3">
-              <input type="checkbox" class="form-check-input" v-model="pdf_isToDoAll" />
-              <label for="exampleFormControlInput1" class="form-check-label"><b>
-                  處理所有頁數
-                </b></label>
-            </div>
-            <div class="mb-3" v-if="pdf_isToDoAll === false">
-              <label for="exampleFormControlInput1" class="form-label"><b>
-                  <BIconColumns style="vertical-align:text-top;" class="icon" /> 跨頁數請用","分隔，如:1,3,5。 某範圍頁數可用 "-"
-                  表示，如:1-3。
-                  亦可搭配使用，如:1-3,5,7
-                </b></label>
-              <input type="text" id="exampleFormControlInput1" class="form-control" placeholder="輸入頁數"
-                v-model="pdf_page">
-            </div> -->
+            <ayx
+              data-ui-props="{type:'FileBrowse', widgetId:'pdfFlie1', browseType:'File', fileTypeFilters: 'PDF Files (*.pdf)|*.pdf'}">
+            </ayx><br>
+            <ayx
+              data-ui-props="{type:'FileBrowse', widgetId:'pdfFlie2', browseType:'File', fileTypeFilters: 'PDF Files (*.pdf)|*.pdf'}">
+            </ayx><br>
+            <ayx
+              data-ui-props="{type:'FileBrowse', widgetId:'pdfFlie3', browseType:'File', fileTypeFilters: 'PDF Files (*.pdf)|*.pdf'}">
+            </ayx><br>
+            <ayx
+              data-ui-props="{type:'FileBrowse', widgetId:'pdfFlie4', browseType:'File', fileTypeFilters: 'PDF Files (*.pdf)|*.pdf'}">
+            </ayx><br>
+            <ayx
+              data-ui-props="{type:'FileBrowse', widgetId:'pdfFlie5', browseType:'File', fileTypeFilters: 'PDF Files (*.pdf)|*.pdf'}">
+            </ayx><br>
+            <ayx
+              data-ui-props="{type:'FileBrowse', widgetId:'pdfFlie6', browseType:'File', fileTypeFilters: 'PDF Files (*.pdf)|*.pdf'}">
+            </ayx><br>
+            <ayx
+              data-ui-props="{type:'FileBrowse', widgetId:'pdfFlie7', browseType:'File', fileTypeFilters: 'PDF Files (*.pdf)|*.pdf'}">
+            </ayx><br>
+            <ayx
+              data-ui-props="{type:'FileBrowse', widgetId:'pdfFlie8', browseType:'File', fileTypeFilters: 'PDF Files (*.pdf)|*.pdf'}">
+            </ayx>
           </div>
         </div>
 
@@ -63,7 +63,7 @@
               <ayx data-ui-props='{type:"FileBrowse", widgetId:"outputPath", browseType:"Folder"}'></ayx>
           </div>
         </div> -->
-        
+
 
       </div>
     </div>
@@ -75,10 +75,6 @@
 
 </template>
 <script>
-import draggable from "vuedraggable";
-//replaceAll Polyfill
-
-
 /**
  * String.prototype.replaceAll() polyfill
  * https://gomakethings.com/how-to-replace-a-section-of-a-string-with-another-one-with-vanilla-js/
@@ -109,21 +105,9 @@ export default {
   name: 'files',
   data() {
     return {
-      str_columns:[],
-      val_columns:[],
-      enabledSortList: true,
-      fileList: [
-        { name: "File1", path: "./test/File1", id: 0 },
-        { name: "File2", path: "./test/File2", id: 1 },
-        { name: "File3", path: "./test/File3", id: 2 }
-      ],
-      dragging: false,
-      pdf_dir: "",
-      showData:""
     }
   },
   components: {
-    draggable
   },
   watch: {
   },
@@ -140,56 +124,47 @@ export default {
         window.Alteryx.Gui.BeforeLoad = function (manager, AlteryxDataItems) {
           var outputPath = new AlteryxDataItems.SimpleString('outputPath')
           manager.addDataItem(outputPath)
-          // Bind to Checkbox widget
           manager.bindDataItemToWidget(outputPath, 'outputPath')
-        }
-        //Load Settings
-        window.Alteryx.Gui.AfterLoad = function (manager) {
-          // this.pdf_dir = manager.getDataItem("pdf_dir").getValue()
 
-          //Load Income Field
-          let str_type = ["String","WString","V_String","V_WString","Date","Time","DateTime"]
-          let val_type = ["Byte","Int16","Int32","Int64","FixedDecimal","Float","Double"]
-          let incomingFields = manager.getIncomingFields()
-          // alert(incomingFields)
-          window.console.log(incomingFields)
-          this.showData = incomingFields;
-          this.str_columns = incomingFields.filter(item => str_type.indexOf(item.strType) > -1).map(item => item.strName)
-          this.val_columns = incomingFields.filter(item => val_type.indexOf(item.strType) > -1).map(item => item.strName)
-        }.bind(this)
+          var pdfFlie1 = new AlteryxDataItems.SimpleString('pdfFlie1')
+          manager.addDataItem(pdfFlie1)
+          manager.bindDataItemToWidget(pdfFlie1, 'pdfFlie1')
+
+          var pdfFlie2 = new AlteryxDataItems.SimpleString('pdfFlie2')
+          manager.addDataItem(pdfFlie2)
+          manager.bindDataItemToWidget(pdfFlie2, 'pdfFlie2')
+
+          var pdfFlie3 = new AlteryxDataItems.SimpleString('pdfFlie3')
+          manager.addDataItem(pdfFlie3)
+          manager.bindDataItemToWidget(pdfFlie3, 'pdfFlie3')
+
+          var pdfFlie4 = new AlteryxDataItems.SimpleString('pdfFlie4')
+          manager.addDataItem(pdfFlie4)
+          manager.bindDataItemToWidget(pdfFlie4, 'pdfFlie4')
+
+          var pdfFlie5 = new AlteryxDataItems.SimpleString('pdfFlie5')
+          manager.addDataItem(pdfFlie5)
+          manager.bindDataItemToWidget(pdfFlie5, 'pdfFlie5')
+
+          var pdfFlie6 = new AlteryxDataItems.SimpleString('pdfFlie6')
+          manager.addDataItem(pdfFlie6)
+          manager.bindDataItemToWidget(pdfFlie6, 'pdfFlie6')
+
+          var pdfFlie7 = new AlteryxDataItems.SimpleString('pdfFlie7')
+          manager.addDataItem(pdfFlie7)
+          manager.bindDataItemToWidget(pdfFlie7, 'pdfFlie7')
+
+          var pdfFlie8 = new AlteryxDataItems.SimpleString('pdfFlie8')
+          manager.addDataItem(pdfFlie8)
+          manager.bindDataItemToWidget(pdfFlie8, 'pdfFlie8')
+
+        }
       }.bind(this)
       //Load Script
       document.head.appendChild(script)
     }
   },
   computed: {
-    legder_status:function(){
-      try{
-        //是否連接資料
-        if ((this.str_columns.length + this.val_columns.length) === 0){
-          throw `未連接明細帳資料`
-        }
-        // //確定 str_columns 至少有 3 項
-        // if (this.str_columns.length < 3){
-        //   throw `僅有 ${this.str_columns.length} 個欄位屬於字串類型，您至少需要日期、傳票編號、科目代碼 3 個欄位。`
-        // }
-        // //一欄式 - 確定 val_columns 至少有 1 項
-        // if (this.ledger_type === true){
-        //   if (this.val_columns.length < 1){
-        //     throw `僅有 ${this.val_columns.length} 個欄位屬於數值類型，一欄式明細帳您至少需要 1 個數值欄位。`
-        //   }
-        // }
-        // //二欄式 - 確定 val_columns 至少有 1 項
-        // else{
-        //   if (this.val_columns.length < 2){
-        //     throw `僅有 ${this.val_columns.length} 個欄位屬於數值類型，二欄式明細帳您至少需要借方與貸方兩個欄位。`
-        //   }
-        // }
-        return true
-      }catch(err){
-        return err
-      }
-    },
   },
   methods: {
     checkMove: function (e) { window.console.log("Future index: " + e.draggedContext.futureIndex) }
